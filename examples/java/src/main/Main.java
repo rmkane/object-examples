897f292cca662bbc3340f30638cf66c37523783a
rmkane <rmkane@proton.me>
2025-03-02 18:21:37 -0500

    Update Java

--- examples/java/src/main/Main.java
+++ examples/java/src/main/Main.java
@@ -1,13 +1,19 @@
+import java.util.List;
+
 import employee.Employee;
 import employee.EmployeeImpl;
 
 // Main entry point for the Java application
 public class Main {
     public static void main(String[] args) {
-        Employee emp1 = new EmployeeImpl("Bob", 75_000);
-        Employee emp2 = new EmployeeImpl("Joe", 50_000);
+        List<Employee> employees = List.of(
+                new EmployeeImpl("Alice", 100_000),
+                new EmployeeImpl("Bob", 75_000),
+                new EmployeeImpl("Charlie", 50_000));
 
-        emp1.displayEmployee();
-        emp2.displayEmployee();
+        // Print the details of each employee
+        for (Employee employee : employees) {
+            employee.displayEmployee();
+        }
     }
 }
