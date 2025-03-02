d3f0be7ee921ecd409d8d95560d5b77250eabdf9
rmkane <rmkane@proton.me>
2025-03-02 18:23:52 -0500

    Update Go

--- examples/go/main.go
+++ examples/go/main.go
@@ -1,12 +1,16 @@
 package main
 
-import pkg "example.com/employee/pkg/employee"
+import "example.com/employee/pkg/employee"
 
 // Main entry point of the program
 func main() {
-	emp1 := pkg.NewEmployee("Bob", 75_000)
-	emp2 := pkg.NewEmployee("Joe", 50_000)
+	employees := []employee.Employee{
+		employee.NewEmployee("Alice", 100_000),
+		employee.NewEmployee("Bob", 75_000),
+		employee.NewEmployee("Charlie", 50_000),
+	}
 
-	emp1.DisplayEmployee()
-	emp2.DisplayEmployee()
+	for _, emp := range employees {
+		emp.DisplayEmployee()
+	}
 }
